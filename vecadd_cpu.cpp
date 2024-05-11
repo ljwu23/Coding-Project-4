fd210b8e757ac5d3c18153919a59eba40c59480e
ljwu23 <lwu17@sfsu.edu>
2024-05-11 10:31:27 -0700

    Initial Commit

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <chrono>
+
 
 // function to add the elements of two arrays
 void add(int n, float *x, float *y)
@@ -22,21 +22,12 @@ int main(void)
         y[i] = 2.0f;
     }
 
-    auto start_timer = std::chrono::steady_clock::now();
-
-    add(N, x, y);
-
-    auto end_timer = std::chrono::steady_clock::now();
-
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_timer - start_timer).count();
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
     for (int i = 0; i < N; i++)
         maxError = std::fmax(maxError, std::fabs(y[i]-3.0f));
     std::cout << "Max error: " << maxError << std::endl;
-    
-    std::cout << "Elapsed time: " << elapsed_time << " milliseconds" << std::endl;
 
     // Free memory
     delete [] x;
